f9f8b37541d892b1f218ba614d99d862a023e7b4
Adeoluwa Idowu <adeoluwa53@gmail.com>
2023-07-20 21:12:11 +0100

    adding context

---
+++ frontend/src/context/workoutContext.js
@@ -0,0 +1,18 @@
+
+import { createContext, useReducer } from "react";
+import { workoutsReducer } from "../reducers/workoutsReducer";
+
+const WorkoutsContext = createContext()
+
+const WorkoutsContextProvider = ({ children }) => {
+
+  const [state, dispatch] = useReducer( workoutsReducer, {workouts : null})
+
+  return(
+    <WorkoutsContext.Provider value={{state, dispatch}}>
+      { children }
+    </WorkoutsContext.Provider>
+  )
+}
+
+export default WorkoutsContextProvider
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WorkoutsContextProvider from './context/workoutContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WorkoutsContextProvider>
+      <App />
+    </WorkoutsContextProvider>
   </React.StrictMode>
 );
 
