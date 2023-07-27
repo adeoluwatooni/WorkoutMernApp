1b68c2742d714565de892f50567a685dc9e97de0
Adeoluwa Idowu <adeoluwa53@gmail.com>
2023-07-28 00:07:52 +0100

    ok

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,4 +1,4 @@
-const { response } = require('express')
+// const { response } = require('express')
 const workoutModel = require('../models/workoutModel')
 const mongoose = require('mongoose')
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 
-
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
@@ -14,15 +14,20 @@ const userSchema = new mongoose.Schema({
   }
 })
 
-// static method
+// static method - for the sign up controller logic
 userSchema.statics.signup = async function (email, password) {
-  const exists = await this.findOne({ email })
   
+  //check if the email is already in use
+  const exists = await this.findOne({ email })
+
   if (exists) {
     throw Error('This email is already is use')
   }
 
-  
+  // hashing user password
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+
 }
 
 module.exports = mongoose.model('User', userSchema)
